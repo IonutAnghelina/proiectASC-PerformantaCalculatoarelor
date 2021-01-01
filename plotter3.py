b8ed83a7a268d87bf4be2d2cbd55e157d8bede29
ionanghelina <45295906+ionanghelina@users.noreply.github.com>
2021-01-02 00:41:22 +0200

    Create plotter3.py

---
+++ plotter3.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+fin=open("maxperf3.txt","r")
+
+l=fin.read().split(sep="\n")
+
+#print(l[0])
+performance=[]
+years=list(range(1994,2021))
+length=len(l)
+#print(l[-1])
+
+for i in range(0,length-1,3):
+    print("{} {} {}".format(l[i],l[i+1],l[i+2]))
+    x1=float(l[i].split()[-1])
+    x2=float(l[i+1].split()[-1])
+    x3=float(l[i+2].split()[-1])
+
+    #print((x1+x2+x3)/3)
+    performance.append((x1+x2+x3)/3)
+plt.plot(years,performance)
+plt.show()
+
+growth=[]
+
+for i in range(1,len(performance)):
+    progress=(performance[i]-performance[i-1])/performance[i-1]
+    growth.append(100*progress)
+
+plt.plot(years[1:],growth)
+plt.show()
